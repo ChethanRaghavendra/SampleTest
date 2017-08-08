c8002546b3c15674e59767af7d818edaa4744325
Chethan Raghavendra <bgragha89@gmail.com>
2017-08-08 09:30:55 +0530

    thrid commit

--- src/test/java/gitTest/GitTest.java
+++ src/test/java/gitTest/GitTest.java
@@ -1,13 +1,23 @@
 package gitTest;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class GitTest {
 
 	@Test
-	public void loginTest(){		
-		
-		
+	public void loginTest() throws IOException{		
+		WebDriver wd = new FirefoxDriver();
+		wd.get("https://www.google.com/");
+		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("E:/Software_Setup files/Java/workspace/GitTest/Screenshot/google.jpg"));
 	}
 	
 }
